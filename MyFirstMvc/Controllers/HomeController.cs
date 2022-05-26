f3e5de6b7cd1f9c19857c38ce33a3a2cdb052c0d
AnanthapalliAvinash <avinashananthapalli11@gmail.com>
2022-05-26 18:40:00 +0530

    Add project files.

---
+++ MyFirstMvc/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using MyFirstMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyFirstMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+         [HttpGet]
+        public ActionResult Index()
+        {
+            Sample product = new Sample();
+            product = product.GetProduct();
+            return View(product);
+
+        }
+        
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            List<Sample> products = Sample.GetAllProducts();
+
+            return View(products);
+        }
+       [NonAction]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        
+    }
+}
---
+++ MyFirstMvc/Models/Sample.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyFirstMvc.Models
+{
+    public class Sample
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int OrderId { get; set; }
+        public int Quantity { get; set; }
+
+        public Sample GetProduct()
+        {
+            Sample product = new Sample() { ProductId = 01, ProductName = "C# Book", OrderId = 02, Quantity = 2 };
+           
+            return product;
+        }
+        public static List<Sample> GetAllProducts()
+        {
+            List<Sample> products = new List<Sample>() {
+        new Sample() { ProductId = 01, ProductName = "C# Book", OrderId = 02, Quantity = 2 },
+        new Sample() { ProductId = 02, ProductName = "ASP.NET Book", OrderId = 02, Quantity = 2 },
+        new Sample() { ProductId = 03, ProductName = "ASP.NET CORE Book", OrderId = 02, Quantity = 2 }
+        
+    };
+            return products;
+        }
+    }
+}
